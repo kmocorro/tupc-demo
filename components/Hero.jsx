1b982178597d11c59fba6d3ba3a6b9cc2e8794d8
Kevin Mocorro <kevinmocorro@gmail.com>
2022-11-14 10:26:08 +0800

    Change campus and mission/vision

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className={styles.container}>
       <h1>Technological University of the Philippines</h1>
-      <h3>Cavite Campus</h3>
+      <h3>Manila Campus</h3>
       <p>CQT Avenue, Salawag, Dasmarinas, Cavite</p>
     </div>
   );
